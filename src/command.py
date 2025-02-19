4f31a74436f1f194edbc70e4b3e04aa6e9318020
tobiasfremming <tobias.fremming@gmail.com>
2025-02-19 16:06:21 +0100

    feat: implement command pattern

---
+++ src/command.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+
+class Command(BaseModel):
+    """Message from the VR application about the current state. This is an loose implementation of the command pattern
+    """
+    user_name: str
+    user_mode: str
+    question: str
+    progress: list[str]
+    user_actions: list[str]
+    NPC: int
+
+
+class Prompt(BaseModel):
+    """Message to be passed to a large language model."""
+    user_name: str
+    user_mode: str
+    question: str
+    progress: list[str]
+    user_actions: list[str]
+    base_prompt: str
+    context: str
+
+def prompt_to_json(prompt: Prompt) -> str:
+    """Converts a Prompt instance to a JSON string."""
+    return prompt.model_dump_json()
