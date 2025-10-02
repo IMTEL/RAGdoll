ccb06eb0dfe85a1df3bc9fe85cd058d3918b0238
dtktran <dtktran@stud.ntnu.no>
2025-10-02 10:55:46 +0200

    feat: Created initial agent and role models.
    Co-authored-by: suzannemaduga <madugasuzanne@gmail.com>

---
+++ src/models/agent.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from .role import Role
+
+class Agent(BaseModel):
+    name: str
+    description: str
+    prompt: str
+    corpa: list[str]
+    roles: list[Role]
+    llm_model: str
+    llm_temperature: float
+    llm_max_tokens: int
+    llm_api_key: str
+    access_key: list[str] # str for now, but may need to change later
+    retrieval_method: str
+    embedding_model: str # idk if will be in agent
+    
+
+
+
+
+
+    
