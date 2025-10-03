0580e25f761f62c7a8106e639354947cf5f88cd5
MrJoeKr <kraus.jozef225@gmail.com>
2025-10-04 01:18:13 +0200

    refactor: simplify Config initialization by removing parameters and using default values

--- src/config.py
+++ src/config.py
@@ -9,21 +9,20 @@ load_dotenv()
 class Config:
     """Configuration class to manage environment variables and model loading."""
 
-    def __init__(
-        self, path=".env", gpt_model="gpt-4o-mini", gemini_model="gemini-2.0-flash-lite"
-    ):
+    def __init__(self, path=".env"):
         self.path = path
         self.ENV = os.getenv("ENV", "dev")
 
         self.MODEL = os.getenv("MODEL", "idun")
-        self.GPT_MODEL = os.getenv("GPT_MODEL", gpt_model)
-        self.GEMINI_MODEL = os.getenv("GEMINI_MODEL", gemini_model)
+        self.GPT_MODEL = os.getenv("GPT_MODEL", "gpt-4o-mini")
+        self.GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.0-flash-lite")
         self.API_KEY = os.getenv("OPENAI_API_KEY", "your_default_api_key")
         self.GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "your_default_gemini_api_key")
-        self.IDUN_API_URL = os.getenv("IDUN_API_URL", "https://idun-llm")
+        self.IDUN_API_URL = os.getenv(
+            "IDUN_API_URL", "https://idun-llm.hpc.ntnu.no/api/chat/completions"
+        )
         self.IDUN_API_KEY = os.getenv("IDUN_API_KEY", "secret_secret")
-        self.IDUN_MODEL = os.getenv("IDUN_MODEL", "gpt-4o-mini")
-        # Load the model (options: tiny, base, small, medium, large)
+        self.IDUN_MODEL = os.getenv("IDUN_MODEL", "openai/gpt-oss-120b")
 
         if self.ENV == "dev":  # TODO: change this to 'dev' when ready
             self.MONGODB_URI = os.getenv(
