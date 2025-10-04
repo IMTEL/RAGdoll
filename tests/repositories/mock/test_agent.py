c8f92ba9adb654cacc8e879926674fd6cbd0682f
MrJoeKr <kraus.jozef225@gmail.com>
2025-10-04 19:45:33 +0200

    refactor: organize repositories tests into directories

--- tests/repositories/mock/test_agent.py
+++ tests/repositories/mock/test_agent.py
@@ -224,9 +224,6 @@ class TestMockAgentRepository:
         assert retrieved_again.name == original_name
 
 
-# TODO: Add MongoDBAgentRepository tests!
-
-
 class TestFactoryIntegration:
     """Test that the factory returns the correct repository based on configuration."""
 
