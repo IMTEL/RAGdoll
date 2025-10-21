319d9d1c415f094605b775b1da38a9e621cfd5b5
MrJoeKr <kraus.jozef225@gmail.com>
2025-10-21 16:22:34 +0200

    fix: remove unused pytest sessionstart function

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,3 @@ def pytest_configure(config):
     # Check if RUNNING_TESTS is set to True
     if not RUNNING_TESTS:
         pytest.exit("RUNNING_TESTS is not set to True. Exiting tests.")
-
-
-def pytest_sessionstart(session):
-    # Code to run before tests start
-    print("Preparing to run tests...")
