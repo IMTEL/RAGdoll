e40727c9ef51ad8c8d708d22fa264bb1f52423be
tobiasfremming <tobias.fremming@gmail.com>
2025-12-04 17:38:19 +0100

    feat: add .dockerignore file to exclude unnecessary files from Docker context

--- src/auth/auth_service/open_auth_service.py
+++ src/auth/auth_service/open_auth_service.py
@@ -14,7 +14,8 @@ It returns a default 'user' account that has access to all agents.
 
 
 class OpenAuthService(BaseAuthService):
-    DEFAULT_USER_ID = "default_user"  # Consistent user ID
+    # We'll use provider_user_id to identify our default user
+    DEFAULT_PROVIDER_USER_ID = "noauth_default_user"
     
     def __init__(self, user_db: UserDao, agent_db: AgentDAO):
         self.user_db = user_db
@@ -27,28 +28,34 @@ class OpenAuthService(BaseAuthService):
         This user is named 'user' and has access to all agents in the system.
         The user is cached to avoid unnecessary database calls.
         """
-        # Try to get existing user from database
-        user = self.user_db.get_user_by_id(self.DEFAULT_USER_ID)
+        # Try to get existing user from database by provider_user_id
+        # We need to scan for it since we don't have the MongoDB _id
+        users = list(self.user_db.collection.find({"provider_user_id": self.DEFAULT_PROVIDER_USER_ID}))
         
-        if user is None:
+        if users:
+            # User exists, convert to User object
+            user_doc = users[0]
+            user_doc.pop("_id", None)
+            user = User(**user_doc)
+        else:
             # Create default user if it doesn't exist
             agents = self.agent_db.get_agents()
             agent_ids = [agent.id for agent in agents]
             user = User(
-                id=self.DEFAULT_USER_ID,
                 name="user",  # Simple name: "user"
                 auth_provider="noauth",
-                provider_user_id=self.DEFAULT_USER_ID,
+                provider_user_id=self.DEFAULT_PROVIDER_USER_ID,
                 owned_agents=agent_ids,
             )
+            # Don't set id - let MongoDB create it
             user = self.user_db.set_user(user)
-        else:
-            # Update existing user to own all current agents
-            agents = self.agent_db.get_agents()
-            agent_ids = [agent.id for agent in agents]
-            if set(user.owned_agents) != set(agent_ids):
-                user.owned_agents = agent_ids
-                user = self.user_db.set_user(user)
+        
+        # Always update to ensure user owns all current agents
+        agents = self.agent_db.get_agents()
+        agent_ids = [agent.id for agent in agents]
+        if set(user.owned_agents) != set(agent_ids):
+            user.owned_agents = agent_ids
+            user = self.user_db.set_user(user)
         
         return user
 
